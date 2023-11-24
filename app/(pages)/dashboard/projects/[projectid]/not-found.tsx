7a4c79fc4ee82244f29028e3425ad5b31bef77a7
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-11-24 10:20:28 +0100

    My first server component requests on my own.  :')

---
+++ app/(pages)/dashboard/projects/[projectid]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="h-screen w-full flex justify-center items-center">
+        <h1>Could not find requested project.</h1>
+      </div>
+    </>
+  );
+}
--- app/(pages)/dashboard/projects/[projectid]/page.tsx
+++ app/(pages)/dashboard/projects/[projectid]/page.tsx
@@ -1,4 +1,7 @@
-export default function Project({
+import { sql } from "@vercel/postgres";
+import { notFound } from "next/navigation";
+
+export default async function Project({
   params,
 }: {
   params: {
@@ -6,10 +9,23 @@ export default function Project({
   };
 }) {
   const projectid = params.projectid;
+  const { rows } = await sql`
+    SELECT * FROM Projects 
+    WHERE project_id=${projectid}
+  `;
+
+  if (!rows[0]) {
+    notFound();
+  }
+
   return (
     <>
       <div className="h-screen w-full flex justify-center items-center">
-        <h1>Project Page for ProjectID #{projectid}</h1>
+        <div className="text-center">
+          {/* <h1>Project Page for ProjectID #{projectid}</h1> */}
+          <h1>Project Page for ProjectID #{rows[0].project_id}</h1>
+          <p className="pt-2">{rows[0].project_name}</p>
+        </div>
       </div>
     </>
   );
