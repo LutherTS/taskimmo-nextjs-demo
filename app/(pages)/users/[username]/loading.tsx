bc27df44c8c6cc6f01f8bae2725a1cbdf30e3710
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-11-24 12:24:48 +0100

    Added loading for users/[username] routes.

---
+++ app/(pages)/users/[username]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="min-h-screen p-8 w-full flex justify-center items-center">
+        <h1>Loading...</h1>
+      </div>
+    </>
+  );
+}
